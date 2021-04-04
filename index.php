3559b1229c6bee0295b0101827569517f1995f56
lexexd <75400601+lexexd@users.noreply.github.com>
2021-04-04 17:48:03 -0400

    proyecto

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
+        <link rel="stylesheet" href="./css/mdb.min.css">
+        <!-- Plugin file -->
+        <link rel="stylesheet" href="./css/addons/datatables.min.css">
+        <link rel="stylesheet" href="./css/style.css">
+        <title>Inicio</title>
+      </head>
+<body>
+    
+<!--Navbar -->
+<header>
+    <nav class="mb-1 navbar navbar-expand-sm navbar-dark rojito  navbar-nav fixed-top navbar-brand scrolling-navbar " >
+      <a class="navbar-brand" href="">SEX-LIMA</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4"
+        aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse " id="navbarSupportedContent-4">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="file:///C:/Users/user/Desktop/proyectoshtml/sexlima/profile.html">
+              <i class="fas fa-check-double"></i> Perfiles
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="file:///C:/Users/user/Desktop/proyectoshtml/sexlima/index.html">
+              <i class="fas fa-home" style="background: transparent;"></i> Inicio</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="file:///C:/Users/user/Desktop/proyectoshtml/sexlima/contacto.html">
+              <i class="far fa-star"></i> Contacto </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+</header>
+  <!-- primer banner-->
+<div style="background-color: #F06345;" class="fixed-bavbar">
+    <div style="text-align: center;">
+      <h1> Pensando en ti, solo perfiles verificados </h1>
+      <i class="fas fa-check-double"></i>
+    </div>
+  </div>
+  <!-- primer banner-->
+
+
+
+
+
+<!-- primer banner-->
+<div style="background-color: #F06345;" class="fixed-bavbar">
+    <div style="text-align: center;">
+      <h1> Pensando en ti, solo perfiles verificados </h1>
+      <i class="fas fa-check-double"></i>
+    </div>
+  </div>
+  <!-- primer banner-->
+
+<!--Carousel Wrapper-->
+<!--Carousel Wrapper-->
+<div class="container">
+<div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+  <!--Indicators-->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+    <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+  </ol>
+  <!--/.Indicators-->
+  <!--Slides-->
+  <div class="carousel-inner" >
+    <!--First slide-->
+    <div class="carousel-item active">
+      <img class="d-block w-100 " src="./imagenes/rubi/imagen4.jpg" 
+        alt="First slide">
+    </div>
+    <!--/First slide-->
+    <!--Second slide-->
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg"
+        alt="Second slide">
+    </div>
+    <!--/Second slide-->
+    <!--Third slide-->
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
+        alt="Third slide">
+    </div>
+    <!--/Third slide-->
+  </div>
+  <!--/.Slides-->
+  <!--Controls-->
+  <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  <!--/.Controls-->
+</div>
+</div>
+<!--Carousel Wrapper-->
+  <!--Carousel Wrapper-->
+
+
+   <div class="card top-nav-collapse">
+      <div class="card-body" style="text-align: center;">
+        <h1> EXCLUSIVO PARA TI </h1>
+      </div>
+    </div>
+
+<!-- Cartas -->
+<div class="card-group">
+    <div class="card">
+        <!-- primer carta-->
+      <img
+        src="./imagenes/rubi/Imagen5.jpg"class="card-img-top"
+        alt="..."
+      />
+      
+      <div class="card-body">
+        <h5 class="card-title">Rubi</h5>
+        <p class="card-text">Venezolana breve descripcion
+        </p>
+        <p class="card-text">
+          
+            <a href="file:///C:/Users/user/Desktop/proyectoshtml/sexlima/profiles/rubi.html">
+              <button type="button" class="btn btn-danger">Perfil</button>
+            </a>
+          
+          <small class="text-muted">recien llego</small>
+        </p>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        src="https://mdbootstrap.com/img/new/standard/city/042.jpg"
+        class="card-img-top"
+        alt="..."
+      />
+      <!-- segunda carta-->
+      <div class="card-body">
+        <h5 class="card-title">Rubi</h5>
+        <p class="card-text">Venezolana breve descripcion
+        </p>
+        <p class="card-text">
+          
+            <a href="file:///C:/Users/user/Desktop/proyectoshtml/sexlima/profiles/rubi.html">
+              <button type="button" class="btn btn-danger">Perfil</button>
+            </a>
+          
+          <small class="text-muted">recien llego</small>
+        </p>
+      </div>
+    </div>
+    <div class="card">
+      <img
+        src="https://mdbootstrap.com/img/new/standard/city/043.jpg"
+        class="card-img-top"
+        alt="..."
+      />
+      <!-- tercer carta-->
+      <div class="card-body">
+        <h5 class="card-title">Rubi</h5>
+        <p class="card-text">Venezolana breve descripcion
+        </p>
+        <p class="card-text">
+          
+            <a href="file:///C:/Users/user/Desktop/proyectoshtml/sexlima/profiles/rubi.html">
+              <button type="button" class="btn btn-danger">Perfil</button>
+            </a>
+          
+          <small class="text-muted">recien llego</small>
+        </p>
+      </div>
+    </div>
+  </div>
+<!-- Cartas -->
+
+<footer class="text-center text-white" style="background-color: #FF5733;">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!-- Section: Images -->
+    <section class="">
+      <div class="row">
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light">
+            <a href="#!"></a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >              </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+          <div
+            class="bg-image hover-overlay ripple shadow-1-strong rounded"
+            data-ripple-color="light"
+          >
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Section: Images -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    Espacio Publicitario
+    <a class="text-white" href="">Sex-lima complaciendo desde siempre</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+  <script type="text/javascript" src="./js/jquery.min.js"></script>
+  <script type="text/javascript" src="./js/popper.min.js"></script>
+  <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="./js/mdb.min.js"></script>
+  <!-- Plugin file -->
+  <script src="./js/addons/datatables.min.js"></script>
+
+
+
+
+
+</body>
+</html>
